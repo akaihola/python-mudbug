3e1c965410091b49caf928ea5ba432647950abb8
Antti Kaihola <akaihol+mudbug@ambitone.com>
2009-04-08 22:12:46 +0300

    Fixed setup script

--- setup.py
+++ setup.py
@@ -7,9 +7,8 @@ setup(
     author_email='akaihol+mudbug@ambitone.com',
     version=VERSION,
     url='http://github.com/akaihola/mudbug',
-    py_modules=['mudbug'],
+    packages=['mudbug'],
     description='MS Access JET engine MDB file helpers',
-    description_full=__doc__,
     classifiers=[
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python'
